9c0bbc9e3eb01ad13579e7d33652915b37ae5647
taylous <taylous99@gmail.com>
2024-04-29 12:44:35 +0900

    [feature] 58

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -44,10 +44,14 @@ class GameTest {
         game.question = "123";
         GuessResult result = game.guess("123");
 
+        boolean solved = true;
+        int strikes = 3;
+        int balls = 0;
+
         assertThat(result).isNotNull();
-        assertThat(result.isSolved()).isEqualTo(true);
-        assertThat(result.getStrikes()).isEqualTo(3);
-        assertThat(result.getBalls()).isEqualTo(0);
+        assertThat(result.isSolved()).isEqualTo(solved);
+        assertThat(result.getStrikes()).isEqualTo(strikes);
+        assertThat(result.getBalls()).isEqualTo(balls);
     }
 
     @Test
@@ -55,9 +59,13 @@ class GameTest {
         game.question = "123";
         GuessResult result = game.guess("456");
 
+        boolean solved = false;
+        int strikes = 0;
+        int balls = 0;
+
         assertThat(result).isNotNull();
-        assertThat(result.isSolved()).isEqualTo(false);
-        assertThat(result.getStrikes()).isEqualTo(0);
-        assertThat(result.getBalls()).isEqualTo(0);
+        assertThat(result.isSolved()).isEqualTo(solved);
+        assertThat(result.getStrikes()).isEqualTo(strikes);
+        assertThat(result.getBalls()).isEqualTo(balls);
     }
 }
