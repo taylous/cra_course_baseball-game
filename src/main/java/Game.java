f0d65cfb703073509b96f4e1684922fde3b0176b
taylous <taylous99@gmail.com>
2024-04-29 11:03:28 +0900

    [feature] 42

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,18 +1,24 @@
 public class Game {
 
     public void guess(String guessNumber) {
-        if(guessNumber == null) {
+        if (guessNumber == null) {
             throw new IllegalArgumentException();
         }
 
-        if(guessNumber.length() != 3) {
-            throw  new IllegalArgumentException();
+        if (guessNumber.length() != 3) {
+            throw new IllegalArgumentException();
         }
 
-        for(char number : guessNumber.toCharArray()) {
-            if(number < '0' || '9' < number) {
-                throw  new IllegalArgumentException();
+        for (char number : guessNumber.toCharArray()) {
+            if (number < '0' || '9' < number) {
+                throw new IllegalArgumentException();
             }
+        }
+
+        if (guessNumber.charAt(0) == guessNumber.charAt(1)
+                || guessNumber.charAt(0) == guessNumber.charAt(2)
+                || guessNumber.charAt(1) == guessNumber.charAt(2)) {
+            throw new IllegalArgumentException();
         }
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -33,5 +33,6 @@ class GameTest {
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
         assertIllegalArgument("12s");
+        assertIllegalArgument("121");
     }
 }
